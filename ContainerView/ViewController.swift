d768a84dbf0f979a11f0197757cfcbe7284cbc93
narezkumar <nareshkumar@Saranyaaaaa.local>
2016-02-18 17:22:09 +0530

    devtree
    devtree

--- ContainerView/ViewController.swift
+++ ContainerView/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var containerViewA: UIView!
     weak var currentViewController: UIViewController?
-
+    //outlet
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
